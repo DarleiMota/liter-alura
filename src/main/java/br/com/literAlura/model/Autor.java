e1647bb0282cc407904eca9c8b4d71293d7da6a4
Darlei Mota <darleichans1@gmail.com>
2025-07-27 12:57:48 -0300

    chore: inicializa projeto Spring Boot com estrutura base

---
+++ src/main/java/br/com/literAlura/model/Autor.java
@@ -0,0 +1,4 @@
+package br.com.literAlura.model;
+
+public class Autor {
+}
---
+++ src/main/java/br/com/literAlura/model/Book.java
@@ -0,0 +1,57 @@
+package br.com.literAlura.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+
+    @Column(name = "download_count")
+    private Integer downloadCount;
+
+    public Book(){}
+
+    public Book(String title, Integer downloadCount) {
+        this.title = title;
+        this.downloadCount = downloadCount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getDownloadCount() {
+        return downloadCount;
+    }
+
+    public void setDownloadCount(Integer downloadCount) {
+        this.downloadCount = downloadCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", downloadCount=" + downloadCount +
+                '}';
+    }
+}
